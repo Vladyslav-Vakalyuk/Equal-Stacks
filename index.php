ecb3972ce2c1ebcb73131cd654676705aab14404
Vladyslav-Vakalyuk <vlad.vakalyk@gmail.com>
2021-03-01 22:29:08 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <?php
 
 /*
@@ -11,49 +10,51 @@
  *  3. INTEGER_ARRAY h3
  */
 
-function equalStacks($h1, $h2, $h3) {
-    
-    $h1Comb = getAllComb($h1);
-    $h2Comb = getAllComb($h2);
-    $h3Comb = getAllComb($h3);
-    $result = array_intersect($h1Comb, $h2Comb, $h3Comb);
-    return max($result);
+function equalStacks( $h1, $h2, $h3 ) {
+
+	$h1Comb = getAllComb( $h1 );
+	$h2Comb = getAllComb( $h2 );
+	$h3Comb = getAllComb( $h3 );
+	$result = array_intersect( $h1Comb, $h2Comb, $h3Comb );
+
+	return max( $result );
 }
 
-function getAllComb($array){
-    $ones = $twos = $array;
-    foreach($ones as $one){
-    foreach($twos as $two){
-    $result[] = $one + $two;
+function getAllComb( $array ) {
+	$ones = $twos = $array;
+	foreach ( $ones as $one ) {
+		foreach ( $twos as $two ) {
+			$result[] = $one + $two;
+		}
+	}
+
+	return $result;
 }
-}
-return $result;
-}
 
-$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+$fptr = fopen( getenv( "OUTPUT_PATH" ), "w" );
 
-$first_multiple_input = explode(' ', rtrim(fgets(STDIN)));
+$first_multiple_input = explode( ' ', rtrim( fgets( STDIN ) ) );
 
-$n1 = intval($first_multiple_input[0]);
+$n1 = intval( $first_multiple_input[0] );
 
-$n2 = intval($first_multiple_input[1]);
+$n2 = intval( $first_multiple_input[1] );
 
-$n3 = intval($first_multiple_input[2]);
+$n3 = intval( $first_multiple_input[2] );
 
-$h1_temp = rtrim(fgets(STDIN));
+$h1_temp = rtrim( fgets( STDIN ) );
 
-$h1 = array_map('intval', preg_split('/ /', $h1_temp, -1, PREG_SPLIT_NO_EMPTY));
+$h1 = array_map( 'intval', preg_split( '/ /', $h1_temp, - 1, PREG_SPLIT_NO_EMPTY ) );
 
-$h2_temp = rtrim(fgets(STDIN));
+$h2_temp = rtrim( fgets( STDIN ) );
 
-$h2 = array_map('intval', preg_split('/ /', $h2_temp, -1, PREG_SPLIT_NO_EMPTY));
+$h2 = array_map( 'intval', preg_split( '/ /', $h2_temp, - 1, PREG_SPLIT_NO_EMPTY ) );
 
-$h3_temp = rtrim(fgets(STDIN));
+$h3_temp = rtrim( fgets( STDIN ) );
 
-$h3 = array_map('intval', preg_split('/ /', $h3_temp, -1, PREG_SPLIT_NO_EMPTY));
+$h3 = array_map( 'intval', preg_split( '/ /', $h3_temp, - 1, PREG_SPLIT_NO_EMPTY ) );
 
-$result = equalStacks($h1, $h2, $h3);
+$result = equalStacks( $h1, $h2, $h3 );
 
-fwrite($fptr, $result . "\n");
+fwrite( $fptr, $result . "\n" );
 
-fclose($fptr);
+fclose( $fptr );
